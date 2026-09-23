a0a70e6a7b6c3924e5be966a032896ff8a7a6599
66050545-Kanyapak <66050545@kmitl.ac.th>
2026-09-23 21:05:44 +0700

    add github

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+first_name = "Kanyapak"
+last_name = "Yongpisalpop"
+
+print(first_name)
+print(last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Kanyapak"
+last_name = "Yongpisalpop"
+whole_name = last_name + " " + first_name
+print(whole_name)
+
